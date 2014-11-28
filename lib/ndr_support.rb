0305a404449ecb8f4a8c55d679c29acec1a02da3
tim.gentry <tim.gentry@153ec483-9fa4-4787-9925-b21d4235b228>
2014-11-28 13:58:06 +0000

    # Dropped in Hash extensions
    git-svn-id: https://deepthought/svn/non-era/gems/ndr_support@2564 153ec483-9fa4-4787-9925-b21d4235b228

--- lib/ndr_support.rb
+++ lib/ndr_support.rb
@@ -1,5 +1,6 @@
 require 'ndr_support/version'
 require 'ndr_support/daterange'
+require 'ndr_support/hash'
 require 'ndr_support/ourdate'
 require 'ndr_support/ourtime'
 require 'ndr_support/fixnum/calculations'
---
+++ lib/ndr_support/hash.rb
@@ -0,0 +1,49 @@
+class Hash
+  # Special intersection method allowing us to intersect a hash with 
+  # an array of keys. Matches string and symbol keys (like stringify_keys)
+  #
+  # For example
+  # {:a => 1, :b => :two, 'c' => '3'} & [:a, :c]
+  #=> {:a => 1, 'c' => '3'}
+  def &(*keys)
+    h = {}
+    self.each { |k, v| h[k] = v if keys.flatten.map(&:to_s).include?(k.to_s) }
+    h
+  end
+  
+  # This method allows us to walk the path of nested hashes to reference a value
+  # 
+  # For example, 
+  # my_hash = { 'one' => '1', 'two' => { 'twopointone' => '2.1', 'twopointtwo' => '2.2' } }
+  # my_hash['one']                 becomes  my_hash.value_by_path('one')
+  # my_hash['two']['twopointone']  becomes  my_hash.value_by_path('two', 'twopointone')
+  # 
+  def value_by_path(first_key, *descendant_keys)
+    result = self[first_key]
+    descendant_keys.each do |key|
+      result = result[key]
+    end
+    result
+  end
+  
+  # This method merges this hash with another, but also merges the :rawtext
+  # (rather than replacing the current hashes rawtext with the second).
+  # Additionally it can raise a RuntimeError to prevent the second hash
+  # overwriting the value for a key from the first.
+  def rawtext_merge(hash2, prevent_overwrite = true)
+    hash1_rawtext = self[:rawtext] || {}
+    hash2_rawtext = hash2[:rawtext] || {}
+    
+    if prevent_overwrite
+      non_unique_rawtext_keys = hash1_rawtext.keys & hash2_rawtext.keys
+      raise(RuntimeError, "Non-unique rawtext keys: #{non_unique_rawtext_keys.inspect}") unless non_unique_rawtext_keys.empty?
+      non_unique_non_rawtext_keys = (self.keys & hash2.keys) - [:rawtext]
+      raise(RuntimeError, "Non-unique non-rawtext keys: #{non_unique_non_rawtext_keys.inspect}") unless non_unique_non_rawtext_keys.empty?
+    end
+    
+    return self.merge(hash2).merge({
+      :rawtext => hash1_rawtext.merge(hash2_rawtext)
+    })
+  end
+  
+end
