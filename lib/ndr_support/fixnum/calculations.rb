f7dbfe2acd1b067858867d58c77e49b03ea78a1d
tim.gentry <tim.gentry@153ec483-9fa4-4787-9925-b21d4235b228>
2014-11-28 13:58:05 +0000

    # Simple rubocop code update
    git-svn-id: https://deepthought/svn/non-era/gems/ndr_support@2563 153ec483-9fa4-4787-9925-b21d4235b228

--- lib/ndr_support/fixnum/calculations.rb
+++ lib/ndr_support/fixnum/calculations.rb
@@ -1,15 +1,15 @@
 class Fixnum
-    # Gets binomial coefficients:
-    #
-    #   4.choose(2) #=> 6
-    #
-    def choose(k)
-      raise(ArgumentError, "cannot choose #{k} from #{self}") unless (0..self) === k
-      self.factorial / ( k.factorial * (self - k).factorial )
-    end
-    
-    def factorial
-      raise(RuntimeError, "cannot calculate #{self}.factorial") unless self >= 0 # limited implementation
-      self.zero? ? 1 : (1..self).inject { |product, i| product*i }
-    end
+  # Gets binomial coefficients:
+  #
+  #   4.choose(2) #=> 6
+  #
+  def choose(k)
+    fail(ArgumentError, "cannot choose #{k} from #{self}") unless (0..self) === k
+    self.factorial / (k.factorial * (self - k).factorial)
+  end
+
+  def factorial
+    fail("cannot calculate #{self}.factorial") unless self >= 0 # limited implementation
+    self.zero? ? 1 : (1..self).inject { |product, i| product * i }
+  end
 end
