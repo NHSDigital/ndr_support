45fec227344c89a49ee88bf91645c77ea84b929d
tim.gentry <tim.gentry@153ec483-9fa4-4787-9925-b21d4235b228>
2014-11-28 13:57:52 +0000

    # Added deny assertion test helper method
    git-svn-id: https://deepthought/svn/non-era/gems/ndr_support@2553 153ec483-9fa4-4787-9925-b21d4235b228

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,10 @@
 require 'test/unit'
 require 'active_support/test_case'
 require 'ndr_support'
+
+class ActiveSupport::TestCase
+  # A useful helper to make 'assert !condition' statements more readable
+  def deny(condition, message='No further information given')
+    assert !condition, message
+  end
+end
