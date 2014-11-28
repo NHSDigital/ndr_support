3ccd0c515a740426a515711b5b1238be944db03c
tim.gentry <tim.gentry@153ec483-9fa4-4787-9925-b21d4235b228>
2014-11-28 13:58:04 +0000

    # Exect copy of Fixnum#choose and Fixnum#factorial
    git-svn-id: https://deepthought/svn/non-era/gems/ndr_support@2562 153ec483-9fa4-4787-9925-b21d4235b228

--- lib/ndr_support.rb
+++ lib/ndr_support.rb
@@ -2,6 +2,7 @@ require 'ndr_support/version'
 require 'ndr_support/daterange'
 require 'ndr_support/ourdate'
 require 'ndr_support/ourtime'
+require 'ndr_support/fixnum/calculations'
 
 module NdrSupport
   # Your code goes here...
---
+++ test/fixnum_test.rb
@@ -0,0 +1,27 @@
+require 'test_helper'
+
+class FixnumTest < ActiveSupport::TestCase
+  test "Fixnums should be extended with #factorial" do
+    assert 1.respond_to?(:factorial)
+  end
+
+   test "Fixnums#factorial should behave correctly" do
+    assert_equal 1, 0.factorial
+    assert_equal 24, 4.factorial
+    assert_raises(RuntimeError) { -1.factorial }
+  end
+
+  test "Fixnums should be extended with #choose" do
+    assert 1.respond_to?(:choose)
+  end
+
+  test "Fixnum#choose should behave correctly" do
+    pascal_row = [1, 5, 10, 10, 5, 1]
+    pascal_row.each_with_index do |target, index|
+      assert_equal target, 5.choose(index)
+    end
+
+    assert_raises(ArgumentError) { 10.choose(11) }
+    assert_raises(ArgumentError) { 10.choose(-1) }
+  end
+end
