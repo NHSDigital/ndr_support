6c994c20fe5d5b52d02a6f2e7e09f13e8cad90b6
tim.gentry <tim.gentry@153ec483-9fa4-4787-9925-b21d4235b228>
2014-11-28 13:58:01 +0000

    # Added Ourtime as is.
    git-svn-id: https://deepthought/svn/non-era/gems/ndr_support@2559 153ec483-9fa4-4787-9925-b21d4235b228

---
+++ lib/ndr_support/ourtime.rb
@@ -0,0 +1,49 @@
+# Convert a string into a time value (timestamp)
+# (helped by String.thetime)
+class Ourtime
+  attr_reader :thetime
+
+  def initialize(x = nil)
+    if x.is_a?(Time)
+      @thetime = x
+    elsif x.is_a?(Date)
+      @thetime = x.to_time
+    elsif x.is_a?(String)
+      self.source = x
+    else
+      @thetime = nil
+    end
+  end
+
+  def to_s
+    @thetime ? @thetime.to_time.to_s(:ui) : ''
+  end
+
+	def empty?
+		# An unspecified time will be empty. A valid or invalid time will not.
+		@thetime.nil? && @source.blank?
+	end
+
+  def source=(s)
+    begin
+      # Re-parse our own timestamps [+- seconds] without swapping month / day
+      @thetime = DateTime.strptime(s, fmt='%d.%m.%Y %H:%M:%S').to_time
+    rescue ArgumentError => e
+      begin
+        @thetime = DateTime.strptime(s, fmt='%d.%m.%Y %H:%M').to_time
+      rescue ArgumentError => e
+        @thetime = Time.parse(s)
+      end
+    end
+    # Apply timezone correction for daylight saving
+    if @thetime
+      @thetime = Ourdate.build_datetime(@thetime.year, @thetime.month,
+                                        @thetime.day, @thetime.hour,
+                                        @thetime.min, @thetime.sec,
+                                        @thetime.instance_of?(Time) ? @thetime.usec : 0).to_time
+    end
+  end
+
+  private :source=
+
+end
