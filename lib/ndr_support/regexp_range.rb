1e613d0e72b69f1ab8f4de1cb06aa4775063f79e
tim.gentry <tim.gentry@153ec483-9fa4-4787-9925-b21d4235b228>
2014-12-05 11:40:30 +0000

    # Added RegexpRange (as is)
    git-svn-id: https://deepthought/svn/non-era/gems/ndr_support@2600 153ec483-9fa4-4787-9925-b21d4235b228

---
+++ lib/ndr_support/regexp_range.rb
@@ -0,0 +1,46 @@
+# This class provides the ability to define a range using numbers or regular expressions
+# and when provided with an array, will return a normal ruby Range object based on the matching
+# elements in the array. NOTE this class is has the same attributes as Range and is identical
+# when serialized (except for the class declaration obviously), but it is NOT a substitute for
+# Range (only a facade).
+class RegexpRange
+  class PatternMatchError < StandardError
+  end
+
+  attr_reader :begin, :end, :excl
+  
+  def initialize(range_start, range_end, exclusive = false)
+    @begin = range_start
+    @end = range_end
+    @excl = exclusive
+  end
+  
+  def to_range(lines)
+    start_line_number = @begin
+    if start_line_number.is_a?(Regexp)
+      lines.each_with_index do |line, i|
+        if line.match(start_line_number)
+          start_line_number = i
+          break
+        end
+      end
+      
+      raise PatternMatchError.new("begin pattern #{start_line_number.inspect} not found") if start_line_number.is_a?(Regexp)
+    end
+    
+    end_line_number = @end
+    if end_line_number.is_a?(Regexp)
+      start_scan_line = start_line_number + 1
+      lines[start_scan_line..-1].each_with_index do |line, i|
+        # puts "##{start_scan_line + i}: #{line}"
+        if line.match(end_line_number)
+          end_line_number = start_scan_line + i
+          break
+        end
+      end
+      raise PatternMatchError.new("end pattern #{end_line_number.inspect} not found on or after line #{start_scan_line}") if end_line_number.is_a?(Regexp)
+    end
+    
+    Range.new(start_line_number, end_line_number, @excl)
+  end
+end
---
+++ test/regexp_range_test.rb
@@ -0,0 +1,88 @@
+require 'test_helper'
+
+class RegexpRangeTest < ActiveSupport::TestCase
+  def setup
+    @lines = [
+      "0Lorem ipsum dolor sit amet",
+      "1consectetur adipisicing elit",
+      "2sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+      "3Ut enim ad minim veniam, quis nostrud exercitation ullamco",
+      "4laboris nisi ut aliquip ex ea commodo consequat"
+    ]
+  end
+
+  test 'to_yaml' do
+    regexp_range = RegexpRange.new(0, /^3Ut/)
+
+    yaml = <<YAML
+--- !ruby/object:RegexpRange 
+begin: 0
+end: !ruby/regexp /^3Ut/
+excl: false
+YAML
+    assert_equal yaml, regexp_range.to_yaml
+
+    deserialized_regexp_range = YAML.load(regexp_range.to_yaml)
+    assert_instance_of RegexpRange, deserialized_regexp_range
+    assert_equal regexp_range.begin, deserialized_regexp_range.begin
+    assert_equal regexp_range.end, deserialized_regexp_range.end
+    assert_equal regexp_range.excl, deserialized_regexp_range.excl
+  end
+
+  test 'to_range with number and number' do
+    assert_equal Range.new(2, 3, true), RegexpRange.new(2, 3, true).to_range(@lines)
+    assert_equal Range.new(2, 3, false), RegexpRange.new(2, 3, false).to_range(@lines)
+    assert_equal Range.new(0, -1, true), RegexpRange.new(0, -1, true).to_range(@lines)
+    assert_equal Range.new(0, -1, false), RegexpRange.new(0, -1, false).to_range(@lines)
+
+    assert_equal @lines[Range.new(2, 3, true)], @lines[RegexpRange.new(2, 3, true).to_range(@lines)]
+    assert_equal @lines[Range.new(2, 3, false)], @lines[RegexpRange.new(2, 3, false).to_range(@lines)]
+  end
+
+  test 'to_range with number and regexp' do
+    assert_equal Range.new(2, 3, true), RegexpRange.new(2, /^3Ut/, true).to_range(@lines)
+    assert_equal Range.new(2, 3, false), RegexpRange.new(2, /^3Ut/, false).to_range(@lines)
+
+    assert_equal @lines[Range.new(2, 3, true)], @lines[RegexpRange.new(2, /^3Ut/, true).to_range(@lines)]
+    assert_equal @lines[Range.new(2, 3, false)], @lines[RegexpRange.new(2, /^3Ut/, false).to_range(@lines)]
+
+    assert_raise RegexpRange::PatternMatchError do
+      RegexpRange.new(2, /^NO_MATCH$/, true).to_range(@lines)
+    end
+    assert_raise RegexpRange::PatternMatchError do
+      RegexpRange.new(2, /^NO_MATCH$/, false).to_range(@lines)
+    end
+  end
+
+  test 'to_range with regexp and number' do
+    assert_equal Range.new(1, -1, true), RegexpRange.new(/^1consec/, -1, true).to_range(@lines)
+    assert_equal Range.new(1, -1, false), RegexpRange.new(/^1consec/, -1, false).to_range(@lines)
+    assert_equal Range.new(1, 5, true), RegexpRange.new(/^1consec/, 5, true).to_range(@lines)
+    assert_equal Range.new(1, 5, false), RegexpRange.new(/^1consec/, 5, false).to_range(@lines)
+
+    assert_equal @lines[Range.new(1, -1, true)], @lines[RegexpRange.new(/^1consec/, -1, true).to_range(@lines)]
+    assert_equal @lines[Range.new(1, -1, false)], @lines[RegexpRange.new(/^1consec/, -1, false).to_range(@lines)]
+
+    assert_raise RegexpRange::PatternMatchError do
+      RegexpRange.new(/^NO_MATCH$/, 5, true).to_range(@lines)
+    end
+    assert_raise RegexpRange::PatternMatchError do
+      RegexpRange.new(/^NO_MATCH$/, 5, false).to_range(@lines)
+    end
+  end
+
+  test 'to_range with regexp and regexp' do
+    assert_equal Range.new(1, 3, true), RegexpRange.new(/^1consec/, /^3Ut/, true).to_range(@lines)
+    assert_equal Range.new(1, 3, false), RegexpRange.new(/^1consec/, /^3Ut/, false).to_range(@lines)
+
+    assert_equal @lines[Range.new(1, 3, true)], @lines[RegexpRange.new(/^1consec/, /^3Ut/, true).to_range(@lines)]
+    assert_equal @lines[Range.new(1, 3, false)], @lines[RegexpRange.new(/^1consec/, /^3Ut/, false).to_range(@lines)]
+
+    assert_raise RegexpRange::PatternMatchError do
+      RegexpRange.new(/^NO_MATCH$/, /^NO_MATCH$/, true).to_range(@lines)
+    end
+    assert_raise RegexpRange::PatternMatchError do
+      RegexpRange.new(/^NO_MATCH$/, /^NO_MATCH$/, false).to_range(@lines)
+    end
+  end
+end
