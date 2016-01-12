8bfb030ce26a4fdcd29fe1ca764b7c90dd817e36
Josh Pencheon <josh.pencheon@googlemail.com>
2016-01-12 16:22:51 +0000

    # Release v3.2.1

--- lib/ndr_support/version.rb
+++ lib/ndr_support/version.rb
@@ -1,5 +1,5 @@
 # This defines the NdrSupport version. If you change it, rebuild and commit the gem.
 # Use "rake build" to build the gem, see rake -T for all bundler rake tasks (and our own).
 module NdrSupport
-  VERSION = '3.2.0'
+  VERSION = '3.2.1'
 end
