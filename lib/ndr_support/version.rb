0cab8a5b04539914556d3aef0d863ff4e051638b
Josh Pencheon <josh.pencheon@googlemail.com>
2016-06-16 10:25:29 +0100

    # tag 4.1.2

--- lib/ndr_support/version.rb
+++ lib/ndr_support/version.rb
@@ -3,5 +3,5 @@
 # This defines the NdrSupport version. If you change it, rebuild and commit the gem.
 # Use "rake build" to build the gem, see rake -T for all bundler rake tasks.
 module NdrSupport
-  VERSION = '4.1.1'
+  VERSION = '4.1.2'
 end
