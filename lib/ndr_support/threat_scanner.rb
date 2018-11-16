63b412ceb0352c20da0fd54e8cb8c9cd5f8ad160
Josh Pencheon <josh.pencheon@googlemail.com>
2018-11-16 16:18:17 +0000

    # Add ThreatScanner, to wrap clamAV

---
+++ lib/ndr_support/threat_scanner.rb
@@ -0,0 +1,64 @@
+require 'English'
+
+# Runs a virus/malware check against the given path, using ClamAV.
+#
+# Sample usage:
+#
+#   # Call with a file object:
+#   ThreatScanner.new(@unknown_tempfile).check!
+#
+#   # ...or with a path:
+#   ThreatScanner.new('path/to/README').check!
+#
+class ThreatScanner
+  class Error < StandardError; end
+
+  class MissingFileError      < Error; end
+  class MissingScannerError   < Error; end
+  class ScannerOperationError < Error; end
+  class ThreatDetectedError   < Error; end
+
+  def self.installed?
+    system('which clamdscan > /dev/null 2>&1')
+  end
+
+  attr_reader :path
+
+  def initialize(path)
+    @path = path.respond_to?(:path) ? path.path : path
+  end
+
+  # Returns true if the given file is deemed safe, and false if it could not
+  # be checked. Raises if a threat is detected, or the file did not exist.
+  def check
+    check!
+  rescue MissingScannerError, ScannerOperationError
+    false
+  end
+
+  # Returns true if the given file is deemed safe, and raises an exception
+  # otherwise (if the file is unsafe / does not exist / scanner broke etc).
+  def check!
+    check_existence! && check_installed! && run_scanner!
+  end
+
+  private
+
+  def check_existence!
+    File.exist?(@path) || raise(MissingFileError, "#{@path} does not exist!")
+  end
+
+  def check_installed!
+    self.class.installed? || raise(MissingScannerError, 'no scanner is available')
+  end
+
+  def run_scanner!
+    `clamdscan --fdpass --quiet #{Shellwords.escape(@path)}`
+
+    case $CHILD_STATUS.exitstatus
+    when 0 then true
+    when 1 then raise(ThreatDetectedError, "possible virus detected at #{@path}!")
+    else        raise(ScannerOperationError, 'the scan was unable to complete')
+    end
+  end
+end
---
+++ test/threat_scanner_test.rb
@@ -0,0 +1,74 @@
+require 'test_helper'
+
+# This tests our ThreatScanner extension
+class ThreatScannerTest < Minitest::Test
+  def setup
+    @tempfile = Tempfile.new
+    @scanner  = ThreatScanner.new(@tempfile)
+
+    stubs(:`)
+  end
+
+  def teardown
+    @tempfile.close!
+  end
+
+  test 'can be initialised with a file' do
+    assert_equal @tempfile.path, @scanner.path
+  end
+
+  test 'can be initialised with a path' do
+    scanner = ThreatScanner.new(@tempfile.path)
+    assert_equal @tempfile.path, scanner.path
+  end
+
+  test 'returns true if no threat is detected (when being strict)' do
+    Process::Status.any_instance.stubs(exitstatus: 0)
+    assert_equal true, @scanner.check!
+  end
+
+  test 'raises if a threat is detected (when being strict)' do
+    Process::Status.any_instance.stubs(exitstatus: 1)
+    assert_raises(ThreatScanner::ThreatDetectedError) { @scanner.check! }
+  end
+
+  test 'raises if the file does not exist (when being strict)' do
+    @tempfile.close!
+    assert_raises(ThreatScanner::MissingFileError) { @scanner.check! }
+  end
+
+  test 'raises if ClamAV is not installed (when being strict)' do
+    ThreatScanner.stubs(installed?: false)
+    assert_raises(ThreatScanner::MissingScannerError) { @scanner.check! }
+  end
+
+  test 'raises if there is an operational error (when being strict)' do
+    Process::Status.any_instance.stubs(exitstatus: 2)
+    assert_raises(ThreatScanner::ScannerOperationError) { @scanner.check! }
+  end
+
+  test 'returns true if no threat is detected (when being relaxed)' do
+    Process::Status.any_instance.stubs(exitstatus: 0)
+    assert_equal true, @scanner.check
+  end
+
+  test 'raises if a threat is detected (when being relaxed)' do
+    Process::Status.any_instance.stubs(exitstatus: 1)
+    assert_raises(ThreatScanner::ThreatDetectedError) { @scanner.check }
+  end
+
+  test 'raises if the file does not exist (when being relaxed)' do
+    @tempfile.close!
+    assert_raises(ThreatScanner::MissingFileError) { @scanner.check }
+  end
+
+  test 'returns false if ClamAV is not installed (when being relaxed)' do
+    ThreatScanner.stubs(installed?: false)
+    assert_equal false, @scanner.check
+  end
+
+  test 'returns false if there is an operational error (when being relaxed)' do
+    Process::Status.any_instance.stubs(exitstatus: 2)
+    assert_equal false, @scanner.check
+  end
+end
