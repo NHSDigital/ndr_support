88733bd2268e625c675d3b7cded15d4880c186fc
Kenny Lee <kennylee@nhs.net>
2026-01-05 14:37:32 +0000

    Bump to v5.11.0 (#64)

--- lib/ndr_support/version.rb
+++ lib/ndr_support/version.rb
@@ -3,5 +3,5 @@
 # This defines the NdrSupport version. If you change it, rebuild and commit the gem.
 # Use "rake build" to build the gem, see rake -T for all bundler rake tasks.
 module NdrSupport
-  VERSION = '5.10.5'
+  VERSION = '5.11.0'
 end
